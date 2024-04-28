0a31be5dfe5a46244b0cc8a69784ea6e214ee07f
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2024-04-28 11:27:29 +0300

    Update

--- {{ cookiecutter.repo_slug }}/tests/project/urls.py
+++ {{ cookiecutter.repo_slug }}/tests/project/urls.py
@@ -1,13 +1,1 @@
-from contextlib import suppress
-
-from django.contrib.auth.models import User
-from django.core.management import call_command
-
-with suppress(Exception):
-    call_command("makemigrations")
-    call_command("migrate")
-    if not User.objects.filter(username="x", email="user@user.com").exists():
-        User.objects.create_superuser(username="x", email="user@user.com", password="x")
-
-
 urlpatterns = []
